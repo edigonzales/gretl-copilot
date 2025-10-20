764ad6fa505883e9a4f7ab1bd8b07aa97463c0ad
edigonzales <edi.gonzales@gmail.com>
2025-10-20 11:26:00 +0200

    start with web app

---
+++ src/main/java/ch/so/agi/gretl/GretlCopilotApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.gretl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GretlCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GretlCopilotApplication.class, args);
+	}
+
+}
